fc6030ad492cf41c5cb26be4beb5b4699d5d125b
unknown <arya.22.beis@acharya.ac.in>
2025-06-06 12:01:58 +0530

    Edit

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -2,6 +2,6 @@ package com.example.app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven edited!");
+        System.out.println("Hello, Maven edited! fff");
     } 
 }
