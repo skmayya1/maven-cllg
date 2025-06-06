d8fd4c3b753fcb670115a265770de8c474176a8d
unknown <arya.22.beis@acharya.ac.in>
2025-06-06 11:55:22 +0530

    first commit

---
+++ src/test/java/com/example/app/AppTest.java
@@ -0,0 +1,11 @@
+package com.example.app;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertEquals(1, 1);
+    }
+}
